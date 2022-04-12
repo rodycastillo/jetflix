45975955b7e99c64e76c764d3cc22c26fa088182
Johguxo <jgonzalesi@uni.pe>
2022-04-12 13:37:25 -0500

    feat: New payment card component  and create hook mercadopago

--- packages/react-jetflix/src/components/RegisterStep3.jsx
+++ packages/react-jetflix/src/components/RegisterStep3.jsx
@@ -1,13 +1,27 @@
 import axios from "axios";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 // import CardPlan from "./CardPlan";
 // import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import useMercadoPago from "../hooks/useMercadoPago";
 // import listPlans from "./list-plans";
 // import { Navigation } from "swiper";
 // import { TablePlan } from "./TablePlan";
 import { RegisterDetailsContext } from "../pages/register/Register";
+import Card from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+
+const initial_state_card = {
+  cvc: "",
+  cardExpirationMonth: "",
+  cardExpirationYear: "",
+  focus: "cardNumber",
+  cardholderName: "",
+  cardNumber: "",
+  issuer: "",
+  installments: 1,
+}
 
 export const RegisterStep3 = (props) => {
   const { registerDetails, setRegisterDetails } = useContext(
@@ -15,20 +29,11 @@ export const RegisterStep3 = (props) => {
   );
   const { username, email, planSelected, password } = registerDetails;
   const history = useHistory();
+
+
   const previousStep = () => {
     props.onSetStep(props.step - 1);
   };
-
-  const componentDidMount = () => {
-    const script = document.createElement("script");
-    script.src = "https://sdk.mercadopago.com/js/v2";
-    script.async = true;
-    document.body.appendChild(script);
-  }
-
-  const payCard = () => {
-    console.log()
-  }
 
   const register = async () => {
     const data = { username, email, planSelected, password };
@@ -39,6 +44,30 @@ export const RegisterStep3 = (props) => {
       console.log(error);
     }
   };
+
+  
+  const [state, setState] = useState(initial_state_card);
+  const resultPayment = useMercadoPago(planSelected.price);
+
+  const handleInputChange = (e) => {
+    setState({
+        ...state,
+        [e.target.dataset.name || e.target.name]: e.target.value,
+      });
+  };
+
+  const handleInputFocus = (e) => {
+      setState({ ...state, focus: e.target.dataset.name || e.target.name });
+  };
+
+  if (resultPayment){
+    if ( 'status' in resultPayment){
+      if (resultPayment.status=='approved') {
+        register();
+      }
+    }
+  }
+  
   return (
     <div className="content-step">
       <div
@@ -61,115 +90,95 @@ export const RegisterStep3 = (props) => {
         </p>
       </div>
       <div className="px-2 sm:px-6 sm:mx-8 lg:px-8 my-16">
-        <div className="flip-container" id="flip-toggle">
-          <div className="flipper">
-            <div className="card-payment front">
-              <div className="card-provider">
-                <div className="visa c-image">
-                  <img src="https://i.imgur.com/NN9t7UH.png" alt="visa" />
+        <div className="container-credit-card container">
+            <Card
+                cvc={state.cvc}
+                expiry={state.cardExpirationMonth + state.cardExpirationYear}
+                name={state.cardholderName}
+                number={state.cardNumber}
+                focused={state.focus}
+                brand={state.issuer}
+            />
+
+            <form id="form-checkout" className="credit-card-form">
+                <div className="form-control">
+                    <input
+                        type="tel"
+                        name="cardNumber"
+                        id="form-checkout__cardNumber"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
                 </div>
-
-                <div className="mastercard c-image">
-                  <img src="https://i.imgur.com/bma7TBS.png" alt="mastercard" />
+                <div className="form-control">
+                    <input
+                        type="tel"
+                        name="cardExpirationMonth"
+                        id="form-checkout__cardExpirationMonth"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
+                    <input
+                        type="tel"
+                        name="cardExpirationYear"
+                        id="form-checkout__cardExpirationYear"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
+                    <input
+                        type="tel"
+                        name="cvc"
+                        id="form-checkout__securityCode"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
                 </div>
-              </div>
-
-              <div className="card-number-wrap">
-                <label className="input-title" >
-                  Card Number*
-                </label>
-                <input
-                  type="text"
-                  name="c-number"
-                  id="c-number"
-                  className="input-field card-number"
-                  placeholder="XXXX XXXX XXXX XXXX"
-                  maxLength="16"
-                  required
-                />
-              </div>
-
-              <div className="cardholder-name-wrap">
-                <label className="input-title cardholder-name">
-                  Name on card*
-                </label>
-                <input
-                  type="text"
-                  name="c-number"
-                  id="c-name"
-                  className="input-field cardholder-name"
-                  placeholder="John Doe"
-                  required
-                />
-              </div>
-
-              <div className="card-expiry-wrap">
-                <label className="input-title">Expiry Date*</label>
-                <div className="custom-dropdown">
-                  <select name="month" className="input-field options">
-                    <option>01</option>
-                    <option>02</option>
-                    <option>03</option>
-                    <option>04</option>
-                    <option>05</option>
-                    <option>06</option>
-                    <option>07</option>
-                    <option>08</option>
-                    <option>09</option>
-                    <option>10</option>
-                    <option>11</option>
-                    <option>12</option>
-                  </select>
+                <div className="form-control">
+                    <input
+                        type="text"
+                        name="cardholderName"
+                        id="form-checkout__cardholderName"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
+                    <input
+                        type="email"
+                        name="cardholderEmail"
+                        id="form-checkout__cardholderEmail"
+                        onFocus={handleInputFocus}
+                    />
                 </div>
-
-                <div className="custom-dropdown">
-                  <select name="year" className="input-field options">
-                    <option>15</option>
-                    <option>16</option>
-                    <option>17</option>
-                    <option>18</option>
-                    <option>19</option>
-                    <option>20</option>
-                    <option>21</option>
-                    <option>22</option>
-                    <option>23</option>
-                    <option>24</option>
-                    <option>24</option>
-                    <option>25</option>
-                  </select>
+                <div className="form-control">
+                    <select
+                        name="issuer"
+                        id="form-checkout__issuer"
+                        on
+                    ></select>
+                    <select
+                        name="identificationType"
+                        id="form-checkout__identificationType"
+                    ></select>
                 </div>
-              </div>
-            </div>
-            <div className="card back">
-              <div className="black-stripe"></div>
-              <div className="cvv-wrap">
-                <label className="input-title">
-                  CVV Code*
-                </label>
-                <input
-                  type="text"
-                  className="input-field cvv-code"
-                  id="cvv-code"
-                  placeholder="000"
-                  maxLength="3"
-                  required
-                />
-              </div>
-              <div className="disclaimer">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                <br />
-                *Champs obligatoires
-              </div>
-            </div>
-          </div>
+                <div className="form-control">
+                    <input
+                        type="text"
+                        name="identificationNumber"
+                        id="form-checkout__identificationNumber"
+                    />
+                </div>
+                <div className="form-control">
+                    <select
+                        name="installments"
+                        id="form-checkout__installments"
+                    ></select>
+                </div>
+                <div className="form-control">
+                    <button type="submit" id="form-checkout__submit">
+                        Pagar
+                    </button>
+                </div>
+            </form>
         </div>
-        <button
-          onClick={() => payCard()}
-          type="submit"
-          className="w-full bg-cyan-600 p-3 font-medium mt-12 text-white"
-        >
-          Pagar
-        </button>
       </div>
     </div>
   );
