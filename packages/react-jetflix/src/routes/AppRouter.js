1ab9e27fd659f49f1d764c21546fe876e88b5bda
Carlito <rody.castillo.tacuri@gmail.com>
2022-04-25 14:52:34 -0500

    WIP

--- packages/react-jetflix/src/routes/AppRouter.js
+++ packages/react-jetflix/src/routes/AppRouter.js
@@ -9,6 +9,7 @@ import { Home } from "../pages/home/Home";
 import { Login } from "../pages/login/Login";
 import { Register } from "../pages/register/Register";
 import { AuthContext } from "../auth/AuthContext";
+import { Watch } from "@material-ui/icons";
 
 export const AppRouter = () => {
   const { user } = useContext(AuthContext);
@@ -17,18 +18,33 @@ export const AppRouter = () => {
       <Router>
         <Switch>
           <Route exact path="/">
-            {user ? <Home typeFormat={''} /> : <Redirect to="/register" />}
+            {user ? <Home typeFormat={""} /> : <Redirect to="/register" />}
           </Route>
           <Route exact path="/add-movie">
-            {user ? <Home typeFormat={'movie'} /> : <Redirect to="/register" />}
+            {user ? <Home typeFormat={"movie"} /> : <Redirect to="/register" />}
           </Route>
           <Route exact path="/add-serie">
-            {user ? <Home typeFormat={'serie'} /> : <Redirect to="/register" />}
+            {user ? <Home typeFormat={"serie"} /> : <Redirect to="/register" />}
           </Route>
           <Route path="/register">
             {!user ? <Register /> : <Redirect to="/" />}
           </Route>
-          <Route path="/login">{!user ? <Login /> : user.isAdmin ? <Redirect to="/add-movie" /> : <Redirect to="/" /> }</Route>
+          <Route path="/login">
+            {!user ? (
+              <Login />
+            ) : user.isAdmin ? (
+              <Redirect to="/add-movie" />
+            ) : (
+              <Redirect to="/" />
+            )}
+          </Route>
+          {user && (
+            <>
+              <Route path="/watch/:id">
+                <Watch />
+              </Route>
+            </>
+          )}
         </Switch>
       </Router>
     </>
